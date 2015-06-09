c5da96c0ab88494e5d9add354f72ca4d56535200
lambdalisue <lambdalisue@hashnote.net>
2015-06-10 07:45:50 +0900

    Start nose_machineout2 to handle PyPI package

---
+++ README.rst
@@ -0,0 +1,14 @@
+nose-machineout2
+==================
+
+``nose-machineout2`` is a fork of nose_machineout_ which originally written by Max Ischenko.
+It seems the original branch (github.com/nvie/nose_machineout) is gone, thus I decied to start maintenance while nose.vim_ won't work on PyPI version.
+
+.. _nose_machineout: https://pypi.python.org/pypi/nose_machineout
+.. _nose.vim: https://github.com/lambdalisue/nose.vim
+
+I added the following features
+
+- Doctest support
+- Python3 support
+- A python package name ``machineout`` is renamed to ``nose_machineout``
--- setup.py
+++ setup.py
@@ -1,38 +1,77 @@
-#!/usr/bin/env python
-"""
-Changes output of the nose testing tool into format easily parsable
-by a machine.
+# coding=utf-8
+import sys
+from setuptools import setup, find_packages
 
-Originally written by Max Ischenko.
-"""
+NAME = 'nose_machineout2'
+VERSION = '0.5.0'
 
-from setuptools import setup
+
+def read(filename):
+    import os
+    BASE_DIR = os.path.dirname(__file__)
+    filename = os.path.join(BASE_DIR, filename)
+    with open(filename, 'r') as fi:
+        return fi.read()
+
+
+def readlist(filename):
+    rows = read(filename).split("\n")
+    rows = [x.strip() for x in rows if x.strip()]
+    return list(rows)
+
+# if we are running on python 3, enable 2to3 and
+# let it use the custom fixers from the custom_fixers
+# package.
+extra = {}
+if sys.version_info >= (3, 0):
+    extra.update(
+        use_2to3=True,
+    )
 
 setup(
-    name="nose_machineout",
-    version="0.4-rc1",
-    description=__doc__.replace('\n', ' ').strip(),
-    author="Mike Crute",
-    author_email="mcrute@gmail.com",
-    url="http://nose-machineout.googlecode.com",
-    install_requires = [
-        "nose>=0.10",
-    ],
-    license="BSD",
-    py_modules=['machineout', 'test_machineout'],
-    entry_points = {
-        'nose.plugins.0.10': [
-            'machineout = machineout:NoseMachineReadableOutput'
-        ],
-    },
-    classifiers=[
+    name=NAME,
+    version=VERSION,
+    description=('Changes output of the nose testing tool into format easily'
+                 'parsable by a machine.'),
+    long_description = read('README.rst'),
+    classifiers = (
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Python Software Foundation License',
         'Operating System :: POSIX',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development',
-    ],
-    keywords='test unittest nose',
-    test_suite = 'nose.collector')
+    ),
+    keywords = 'test unittest nose',
+    author = 'Alisue',
+    author_email = 'lambdalisue@hashnote.net',
+    url = 'https://github.com/lambdalisue/%s' % NAME,
+    download_url = 'https://github.com/lambdalisue/%s/tarball/master' % NAME,
+    license = 'BSD',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    package_data = {
+        '': ['README.rst',
+             'requirements.txt',
+             'requirements-test.txt'],
+    },
+    zip_safe=True,
+    install_requires=readlist('requirements.txt'),
+    test_suite='nose.collector',
+    tests_require=readlist('requirements-test.txt'),
+    entry_points = {
+        'nose.plugins.0.10': [
+            'machineout = nose_machineout:NoseMachineReadableOutput'
+        ],
+    },
+    **extra
+)
